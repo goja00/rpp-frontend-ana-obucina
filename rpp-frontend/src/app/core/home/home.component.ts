10f9658d8085f3dd3863c8569924699f5516d74c
Gojko Grubisic <110236963+goja00@users.noreply.github.com>
2024-03-05 15:41:18 +0100

    homepage

--- rpp-frontend/src/app/core/home/home.component.ts
+++ rpp-frontend/src/app/core/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 
 }
